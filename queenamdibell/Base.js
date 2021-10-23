e5c95a4d0cb695bc6e204bf6442737a3e481a414
KgAmda <91042717+Kgamdaofficial@users.noreply.github.com>
2021-10-23 07:58:08 -0700

    Add files via upload

--- queenamdibell/Base.js
+++ queenamdibell/Base.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Black Amda.
+/* Copyright (C) 2020 Yusuf Usta.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-QueenAmdi - Black Amda
+WhatsAsena - Yusuf Usta
 */
 
 'use strict';
