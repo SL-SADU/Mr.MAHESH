b43a21c6478a1a4c73454d151358cf2e728c2ec7
Black Suis ✅ <91042717+BlackSuis@users.noreply.github.com>
2021-11-08 06:38:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ async function whatsAsena () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2, 2119, 6]
+    conn.version = [2, 2140, 12]
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
