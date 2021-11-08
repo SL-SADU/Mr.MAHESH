6925ed252bb831afc727ecd84cbbe05b7263de9d
Black Suis ✅ <91042717+BlackSuis@users.noreply.github.com>
2021-11-08 08:23:40 -0800

    Update XMedia.js

--- plugins/XMedia.js
+++ plugins/XMedia.js
@@ -1,7 +1,7 @@
 /* Codded by @phaticusthiccy
 Telegram: t.me/phaticusthiccy
 Instagram: www.instagram.com/kyrie.baran
-
+*/
 
 const Asena = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
@@ -1811,4 +1811,4 @@ else if (Config.WORKTYPE == 'public') {
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
-} */
+}
