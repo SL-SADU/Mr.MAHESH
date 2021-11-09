1f6ce91ad575cf4790ac643d5d8d38a1272deaff
Black Suis ✅ <91042717+BlackSuis@users.noreply.github.com>
2021-11-08 16:23:54 -0800

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -52,7 +52,7 @@ if (Config.WORKTYPE == 'private') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'QueenAmdi.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Amdibell.png', mimetype: Mimetype.png});
         await load.delete();
     }));
 }
