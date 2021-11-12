1a7b2d1fe72f83139acd173ea10038a46450ee59
AmdA ✅ <91042717+AmdaOfficial@users.noreply.github.com>
2021-11-12 03:59:28 -0800

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -202,10 +202,10 @@ if (config.WORKTYPE == 'private') {
     
         var mesaj = '';
         arama.all.map((video) => {
-            mesaj += '*💠' + video.title + '* \n👑' + video.url + '\n\n'
+            mesaj += '*💠' + video.title + '* \n👑 ' + video.url + '\n\n'
         });
 
-        await message.client.sendMessage(message.jid,mesaj,MessageType.text);
+        await message.client.sendMessage(message.jid,'   _*=====youtube=====*_\n\n' + mesaj,MessageType.text);
         await reply.delete();
     }));
 
